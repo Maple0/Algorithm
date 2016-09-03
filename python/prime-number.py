82ec4e240a2d668fb35641ca3f3f5e4d9b07c037
zinealone <zinealone@gmail.com>
2016-09-03 22:13:58 +0800

    update prime algorightms

--- python/prime-number.py
+++ python/prime-number.py
@@ -1,8 +1,8 @@
 #Author: Maple0
 #Github:https://github.com/Maple0
 #1st Sep 2016
-#Checking whether a number is prime or not 
 
+#Checking whether a number is prime or not 
 def is_prime1(num):
 	return num>1 and all(num % i for i in range(2, num))
 
@@ -16,6 +16,16 @@ def is_prime2(num):
 
 	return True
 
-n=11
-print(is_prime1(n))
-print(is_prime2(n))
+#Checking if a number can be presented as a sum of two prime numbers
+def get_sum_of_primes(num):
+	primes=[]
+	for x in range(2,int(num/2)+1):
+		if is_prime1(x) and is_prime1(num-x):
+			primes.append(str(num)+" = "+ str(x)+ " + "+str(num-x))
+	return primes
+
+n1=29
+n2=34
+print(is_prime1(n1))
+print(is_prime2(n1))
+print(get_sum_of_primes(n2))
